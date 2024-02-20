78dfa436961e4c14416fd81ccef97cf97a30e999
vinothcl <vinothc@techaffinity.com>
2024-02-20 19:05:01 +0530

    sql changes

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-//Route::get('/register', [App\Http\Controllers\HomeController::class, 'index'])->name('register');
+Route::get('/register', [App\Http\Controllers\HomeController::class, 'index'])->name('register');
 
 Route::get('/logout', function () {
 	Auth::logout();
